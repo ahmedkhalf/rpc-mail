d952cb4d71949db0bb533f36281bf78e6ea31992
Ahmed Khalf <ahmedkhalf567@gmail.com>
2021-03-29 23:36:21 +0400

    Add new proto functions to server

--- main.go
+++ main.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"context"
-	"errors"
 	"log"
 	"net"
 
+	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/client"
 
 	pb "./protomail"
 	"google.golang.org/grpc"
 )
 
-var clientCount uint64 // Used for uid generation
-var clients = make(map[uint64]*client.Client)
-
 type server struct {
 	pb.UnimplementedMailServer
+
+	clientCount uint64 // Used for uid generation
+	clients map[uint64]*client.Client
 }
 
 func (s *server) ConnectClient(ctx context.Context, in *pb.ConnectRequest) (*pb.ConnectResponce, error) {
@@ -33,46 +33,70 @@ func (s *server) ConnectClient(ctx context.Context, in *pb.ConnectRequest) (*pb.
 	}
 
 	if err == nil {
-		clientCount++
-		clients[clientCount] = c
+		s.clientCount++
+		s.clients[s.clientCount] = c
 	}
 
 	log.Println("Connected to", server)
-	return &pb.ConnectResponce{ClientPointer: clientCount}, err
+	return &pb.ConnectResponce{ClientPointer: s.clientCount}, err
 }
 
 func (s *server) LoginClient(ctx context.Context, in *pb.LoginRequest) (*pb.Empty, error) {
 	cid, username, password := in.GetClientPointer(), in.GetUsername(), in.GetPassword()
 	log.Println("Logging in to", username)
 
-	err := clients[cid].Login(username, password)
+	err := s.clients[cid].Login(username, password)
 
 	log.Println("Logged in to", username)
 	return &pb.Empty{}, err
 }
 
-func (s *server) SelectMailbox(ctx context.Context, in *pb.SelectMailboxRequest) (*pb.Empty, error) {
-	cid, name, readonly := in.GetClientPointer(), in.GetName(), in.GetReadonly()
+func (s *server) LogoutClient(ctx context.Context, in *pb.LogoutRequest) (*pb.Empty, error) {
+	cid := in.GetClientPointer()
+	log.Printf("Logging out (client %v)", cid)
 
-	var err error
-	if val, ok := clients[cid]; ok {
-		_, err = val.Select(name, readonly)
-	} else {
-		err = errors.New("cannot call SelectMailbox: client pointer previously destroyed")
-	}
-
-	log.Println("Selected ", name)
+	err := s.clients[cid].Logout()
 
 	return &pb.Empty{}, err
 }
 
-func (s *server) DestroyClient(ctx context.Context, in *pb.DestroyRequest) (*pb.Empty, error) {
+func (s *server) DeleteClient(ctx context.Context, in *pb.DeleteRequest) (*pb.Empty, error) {
 	cid := in.GetClientPointer()
 
-	delete(clients, cid)
+	delete(s.clients, cid)
 	log.Println("Destroyed client", cid)
 
 	return &pb.Empty{}, nil
+}
+
+func (s *server) ListMailboxes(in *pb.ListMailboxesRequest, stream pb.Mail_ListMailboxesServer) error {
+	cid := in.GetClientPointer()
+
+	mailboxes := make(chan *imap.MailboxInfo, 10)
+	done := make(chan error, 1)
+	go func () {
+		done <- s.clients[cid].List("", "*", mailboxes)
+	}()
+
+	for m := range mailboxes {
+		stream.Send(&pb.ListMailboxesResponce{MailboxName: m.Name})
+	}
+
+	if err := <-done; err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+func (s *server) SelectMailbox(ctx context.Context, in *pb.SelectMailboxRequest) (*pb.Empty, error) {
+	cid, name, readonly := in.GetClientPointer(), in.GetMailboxName(), in.GetReadonly()
+
+	_, err := s.clients[cid].Select(name, readonly)
+
+	log.Println("Selected ", name)
+
+	return &pb.Empty{}, err
 }
 
 func main() {
@@ -82,6 +106,7 @@ func main() {
 	}
 
 	s := &server{}
+	s.clients = make(map[uint64]*client.Client)
 
 	grpcServer := grpc.NewServer()
 	pb.RegisterMailServer(grpcServer, s)
