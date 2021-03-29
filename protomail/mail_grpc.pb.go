05277b0716cbbb53424e55ab9ef148ea510a8a39
Ahmed Khalf <ahmedkhalf567@gmail.com>
2021-03-29 22:43:07 +0400

    Update proto with ListMailboxes and LogoutClient messages

--- protomail/mail_grpc.pb.go
+++ protomail/mail_grpc.pb.go
@@ -20,8 +20,10 @@ const _ = grpc.SupportPackageIsVersion7
 type MailClient interface {
 	ConnectClient(ctx context.Context, in *ConnectRequest, opts ...grpc.CallOption) (*ConnectResponce, error)
 	LoginClient(ctx context.Context, in *LoginRequest, opts ...grpc.CallOption) (*Empty, error)
+	LogoutClient(ctx context.Context, in *LogoutRequest, opts ...grpc.CallOption) (*Empty, error)
+	DeleteClient(ctx context.Context, in *DeleteRequest, opts ...grpc.CallOption) (*Empty, error)
+	ListMailboxes(ctx context.Context, in *ListMailboxesRequest, opts ...grpc.CallOption) (Mail_ListMailboxesClient, error)
 	SelectMailbox(ctx context.Context, in *SelectMailboxRequest, opts ...grpc.CallOption) (*Empty, error)
-	DestroyClient(ctx context.Context, in *DestroyRequest, opts ...grpc.CallOption) (*Empty, error)
 }
 
 type mailClient struct {
@@ -50,18 +52,59 @@ func (c *mailClient) LoginClient(ctx context.Context, in *LoginRequest, opts ...
 	return out, nil
 }
 
-func (c *mailClient) SelectMailbox(ctx context.Context, in *SelectMailboxRequest, opts ...grpc.CallOption) (*Empty, error) {
+func (c *mailClient) LogoutClient(ctx context.Context, in *LogoutRequest, opts ...grpc.CallOption) (*Empty, error) {
 	out := new(Empty)
-	err := c.cc.Invoke(ctx, "/protomail.Mail/SelectMailbox", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/protomail.Mail/LogoutClient", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *mailClient) DestroyClient(ctx context.Context, in *DestroyRequest, opts ...grpc.CallOption) (*Empty, error) {
+func (c *mailClient) DeleteClient(ctx context.Context, in *DeleteRequest, opts ...grpc.CallOption) (*Empty, error) {
 	out := new(Empty)
-	err := c.cc.Invoke(ctx, "/protomail.Mail/DestroyClient", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/protomail.Mail/DeleteClient", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *mailClient) ListMailboxes(ctx context.Context, in *ListMailboxesRequest, opts ...grpc.CallOption) (Mail_ListMailboxesClient, error) {
+	stream, err := c.cc.NewStream(ctx, &Mail_ServiceDesc.Streams[0], "/protomail.Mail/ListMailboxes", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &mailListMailboxesClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type Mail_ListMailboxesClient interface {
+	Recv() (*ListMailboxesResponce, error)
+	grpc.ClientStream
+}
+
+type mailListMailboxesClient struct {
+	grpc.ClientStream
+}
+
+func (x *mailListMailboxesClient) Recv() (*ListMailboxesResponce, error) {
+	m := new(ListMailboxesResponce)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (c *mailClient) SelectMailbox(ctx context.Context, in *SelectMailboxRequest, opts ...grpc.CallOption) (*Empty, error) {
+	out := new(Empty)
+	err := c.cc.Invoke(ctx, "/protomail.Mail/SelectMailbox", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -74,8 +117,10 @@ func (c *mailClient) DestroyClient(ctx context.Context, in *DestroyRequest, opts
 type MailServer interface {
 	ConnectClient(context.Context, *ConnectRequest) (*ConnectResponce, error)
 	LoginClient(context.Context, *LoginRequest) (*Empty, error)
+	LogoutClient(context.Context, *LogoutRequest) (*Empty, error)
+	DeleteClient(context.Context, *DeleteRequest) (*Empty, error)
+	ListMailboxes(*ListMailboxesRequest, Mail_ListMailboxesServer) error
 	SelectMailbox(context.Context, *SelectMailboxRequest) (*Empty, error)
-	DestroyClient(context.Context, *DestroyRequest) (*Empty, error)
 	mustEmbedUnimplementedMailServer()
 }
 
@@ -89,11 +134,17 @@ func (UnimplementedMailServer) ConnectClient(context.Context, *ConnectRequest) (
 func (UnimplementedMailServer) LoginClient(context.Context, *LoginRequest) (*Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method LoginClient not implemented")
 }
+func (UnimplementedMailServer) LogoutClient(context.Context, *LogoutRequest) (*Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method LogoutClient not implemented")
+}
+func (UnimplementedMailServer) DeleteClient(context.Context, *DeleteRequest) (*Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DeleteClient not implemented")
+}
+func (UnimplementedMailServer) ListMailboxes(*ListMailboxesRequest, Mail_ListMailboxesServer) error {
+	return status.Errorf(codes.Unimplemented, "method ListMailboxes not implemented")
+}
 func (UnimplementedMailServer) SelectMailbox(context.Context, *SelectMailboxRequest) (*Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SelectMailbox not implemented")
-}
-func (UnimplementedMailServer) DestroyClient(context.Context, *DestroyRequest) (*Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method DestroyClient not implemented")
 }
 func (UnimplementedMailServer) mustEmbedUnimplementedMailServer() {}
 
@@ -144,6 +195,63 @@ func _Mail_LoginClient_Handler(srv interface{}, ctx context.Context, dec func(in
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Mail_LogoutClient_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(LogoutRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MailServer).LogoutClient(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/protomail.Mail/LogoutClient",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MailServer).LogoutClient(ctx, req.(*LogoutRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Mail_DeleteClient_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeleteRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MailServer).DeleteClient(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/protomail.Mail/DeleteClient",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MailServer).DeleteClient(ctx, req.(*DeleteRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Mail_ListMailboxes_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(ListMailboxesRequest)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(MailServer).ListMailboxes(m, &mailListMailboxesServer{stream})
+}
+
+type Mail_ListMailboxesServer interface {
+	Send(*ListMailboxesResponce) error
+	grpc.ServerStream
+}
+
+type mailListMailboxesServer struct {
+	grpc.ServerStream
+}
+
+func (x *mailListMailboxesServer) Send(m *ListMailboxesResponce) error {
+	return x.ServerStream.SendMsg(m)
+}
+
 func _Mail_SelectMailbox_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(SelectMailboxRequest)
 	if err := dec(in); err != nil {
@@ -158,24 +266,6 @@ func _Mail_SelectMailbox_Handler(srv interface{}, ctx context.Context, dec func(
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(MailServer).SelectMailbox(ctx, req.(*SelectMailboxRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _Mail_DestroyClient_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(DestroyRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(MailServer).DestroyClient(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/protomail.Mail/DestroyClient",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(MailServer).DestroyClient(ctx, req.(*DestroyRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -196,14 +286,24 @@ var Mail_ServiceDesc = grpc.ServiceDesc{
 			Handler:    _Mail_LoginClient_Handler,
 		},
 		{
+			MethodName: "LogoutClient",
+			Handler:    _Mail_LogoutClient_Handler,
+		},
+		{
+			MethodName: "DeleteClient",
+			Handler:    _Mail_DeleteClient_Handler,
+		},
+		{
 			MethodName: "SelectMailbox",
 			Handler:    _Mail_SelectMailbox_Handler,
 		},
+	},
+	Streams: []grpc.StreamDesc{
 		{
-			MethodName: "DestroyClient",
-			Handler:    _Mail_DestroyClient_Handler,
+			StreamName:    "ListMailboxes",
+			Handler:       _Mail_ListMailboxes_Handler,
+			ServerStreams: true,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
 	Metadata: "mail.proto",
 }
